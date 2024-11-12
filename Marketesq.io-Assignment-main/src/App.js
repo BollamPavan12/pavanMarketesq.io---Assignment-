32ae54c600d20361b77c963b73af955a47a7436c
Bucky Roberts <bollamBrothers955@gmail.com>
2024-11-12 18:18:50 +0530

    first commit

---
+++ Marketesq.io-Assignment-main/src/App.js
@@ -0,0 +1,23 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import BookingForm from './components/BookingForm';
+import './assets/styles/main.css';
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <BookingForm />
+    </Router>
+  );
+};
+
+export default App;
